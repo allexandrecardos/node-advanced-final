5316e86cc3ab41f20413fdc706e6e167902a9bc4
AlleCardos <dev.allexandrecp@gmail.com>
2024-02-17 23:20:13 -0300

    models update & associations

--- src/models/products.js
+++ src/models/products.js
@@ -3,25 +3,24 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Products extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasMany(models.Products)
+       Products.belongsTo(models.User);
     }
   }
-  User.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    salt: DataTypes.STRING,
-    admin: DataTypes.STRING,
+  Products.init({
+    name: DataTypes.STRING,
+    category: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    UserId: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Products',
   });
-  return User;
+  return Products;
 };
