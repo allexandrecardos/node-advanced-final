4cf0e89ff1248890b91621ee3d2a8239404b6398
AlleCardos <dev.allexandrecp@gmail.com>
2024-02-17 23:17:09 -0300

    products routes & middlewares & request schemas

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-//import packages
 require('dotenv').config();
+
+//requests schemas
+require('./src/utils/schemas');
+
+//import packages
 const express = require('express');
 const multer = require('multer');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser')
 
+//middlewares
+const jwtMiddleware = require('./src/middlewares/token.middleware')
+const authorizationMiddleware = require('./src/middlewares/authorization.middleware')
+
+//routes
 const authRoutes = require("./src/routes/auth.routes");
 const userRoutes = require("./src/routes/user.routes");
+const productsRoutes = require("./src/routes/product.routes");
 
 const app = express();
 const upload = multer();
 
-const PORT = 3000;
+const PORT = process.env.APP_PORT;
 
 //parse application/json
 app.use(express.json());
@@ -22,12 +32,15 @@ app.use(express.urlencoded({ extended: true }));
 //parse application/multipart-form
 app.use(upload.any());
 
-//log HTTP
+//log HTTPs
 app.use(morgan('common'));
 
 app.use(cookieParser())
 
+app.use(['/users', '/products'], jwtMiddleware, authorizationMiddleware)
+
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
+app.use('/products', productsRoutes);
 
 app.listen(PORT, () => console.log(`server running: http://localhost:${PORT}`));
