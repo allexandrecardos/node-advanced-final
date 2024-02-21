f68a905772d858a62adbf88d6b74f9e085261236
AlleCardos <dev.allexandre@gmail.com>
2024-02-21 09:28:53 -0300

    update controllers

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -10,48 +10,45 @@ class ProductController{
         try{
             req.body.UserId = req.user.userId;
             const product = await this.productService.createProduct(req.body);
-            res.status(201).json(product);
+            res.status(201).json({ product: product });
         }catch(err){
-            console.log(err);
+            res.status(500).json({ message: 'Fatal Error!', error: err.message});
         }
     }
 
     async getAllProducts(req, res){
         try{
             const product = await this.productService.getAllProducts();
-            res.status(200).json(product);
+            res.status(200).json({ product: product });
         }catch(err){
-
-
+            res.status(500).json({ message: 'Fatal Error!', error: err.message});
         }
     }
 
     async getProductById(req, res){
         try{
             const product = await this.productService.getProductById(req.params.id);
-            res.status(200).json(product)
+            res.status(200).json({ product: product })
         }catch(err){
-            console.log(err);
+            res.status(500).json({ message: 'Fatal Error!', error: err.message});
         }
     }
 
     async deleteProduct(req, res){
         try{
-            const product = await this.productService.deleteProduct(req.params.id);
-            res.status(200).json(product)
+            await this.productService.deleteProduct(req.params.id);
+            res.status(200).json({ message: 'Product deleted successful!'})
         }catch(err){
-
-
+            res.status(500).json({ message: 'Fatal Error!', error: err.message});
         }
     }
 
     async updateProduct(req, res){
         try{
-            const product = await this.productService.updateProduct(req.params.id, req.body);
-            res.status(200).json(product)
+            await this.productService.updateProduct(req.params.id, req.body);
+            res.status(200).json({ message: 'Product deleted successful!'})
         }catch(err){
-
-
+            res.status(500).json({ message: 'Fatal Error!', error: err.message});
         }
     }
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -35,7 +35,7 @@ class UserController{
 
     async updateUser(req, res){
         try{
-            const user = await this.userService.updateUser(req.params.id, req.body)
+            await this.userService.updateUser(req.params.id, req.body)
             res.status(200).json({ message: 'User updated successful!'})
         }catch(err){
             res.status(500).json({ message: 'Fatal Error!', error: err.message});
